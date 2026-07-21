2061d6ff461b99a41e3dc8d3656e85544a8701f3
Mukherji, Sandeep <sandeep.talk@gmail.com>
2026-07-21 21:21:09 +0530

    feat:added the basic authentication. Trying to create a login now.

--- src/main/java/my/custom/learning/weblearn/controller/UserController.java
+++ src/main/java/my/custom/learning/weblearn/controller/UserController.java
@@ -1,7 +1,9 @@
 package my.custom.learning.weblearn.controller;
 
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +17,42 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import my.custom.learning.weblearn.AppConstants;
-import my.custom.learning.weblearn.service.UserService;
+import my.custom.learning.weblearn.entity.Post;
+import my.custom.learning.weblearn.entity.User;
+import my.custom.learning.weblearn.exceptions.UserNotFoundException;
+import my.custom.learning.weblearn.repository.UserRepository;
 
 @RestController
 public class UserController {
 
 	@Autowired
-	private UserService userService;
+	private UserRepository repository;
 	
-	public UserController(UserService userService) {
+	public UserController(UserRepository repository) {
 		super();
-		this.userService = userService;
+		this.repository = repository;
+	}
+
+	@GetMapping(path = "/users", version=AppConstants.API_VERSION)
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	@GetMapping(path = "/user/{id}", version=AppConstants.API_VERSION)
+	public Optional<User> findById(@PathVariable int id) {
+		Optional<User> user = repository.findById(id);
+		if(user.isEmpty()) {
+			throw new UserNotFoundException("No user found with the user id = "+id, "Line number 30");
+		}
+		return user;
+	}
+	
+	@PostMapping(path = "/user/add", version=AppConstants.API_VERSION)
+	public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		user.setCreated_at(localDateTime);
+		User createUser = repository.save(user);
+		URI location = URI.create("/user/"+createUser.getUser_id());
+		return ResponseEntity.created(location).build();
 	}
 }
--- src/main/java/my/custom/learning/weblearn/repository/UserRepository.java
+++ src/main/java/my/custom/learning/weblearn/repository/UserRepository.java
@@ -1,9 +1,17 @@
 package my.custom.learning.weblearn.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import my.custom.learning.weblearn.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+	// Custom query: user password is stored in the `secret_key` property on the User entity.
+	@Query("SELECT u FROM users u WHERE u.email = :email AND u.secret_key = :password")
+	Optional<User> findByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 
 }
