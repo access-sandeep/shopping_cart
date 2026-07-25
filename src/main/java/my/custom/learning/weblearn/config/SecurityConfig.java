571d685b62243b8abd7dbef4989083c485a0489b
Mukherji, Sandeep <sandeep.talk@gmail.com>
2026-07-25 13:42:38 +0530

    feat: completed the security token generation and usage in the requests

--- src/main/java/my/custom/learning/weblearn/config/SecurityConfig.java
+++ src/main/java/my/custom/learning/weblearn/config/SecurityConfig.java
@@ -1,31 +1,38 @@
 package my.custom.learning.weblearn.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import my.custom.learning.weblearn.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	@Autowired
+	JwtAuthenticationFilter jwtFilter;
+
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.csrf(csrf -> csrf.disable()) // Disable CSRF for testing (enable in production)
-			.authorizeHttpRequests(authz -> authz
-				.requestMatchers("/login").permitAll() // Allow /login without authentication
-				.requestMatchers(
-						"/v3/api-docs", 
-						"/swagger-ui.html", 
-						"/swagger-ui/**", 
-						"/v3/api-docs/**"
-				).permitAll() // Allow Swagger UI
-				.anyRequest().authenticated() // All other requests require authentication
-			)
-			.httpBasic(basic -> {}); // Enable HTTP Basic authentication
-		
+		http.csrf(csrf -> csrf.disable()) // Disable CSRF for testing (enable in production)
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+				.authorizeHttpRequests(auth -> auth
+
+						.requestMatchers("/login").permitAll()
+
+						.requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
+
+						.anyRequest().authenticated())
+
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
 		return http.build();
 	}
 }
