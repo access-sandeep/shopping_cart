e6e45b5bd7fe4055113c19dbb2deffb2ff81817e
Mukherji, Sandeep <sandeep.talk@gmail.com>
2026-08-01 13:23:00 +0530

    feat: category upsert and postman collections

--- src/main/java/my/custom/learning/weblearn/controller/CategoryController.java
+++ src/main/java/my/custom/learning/weblearn/controller/CategoryController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import my.custom.learning.weblearn.AppConstants;
+import my.custom.learning.weblearn.entity.Brand;
 import my.custom.learning.weblearn.entity.CategoryOfProducts;
 import my.custom.learning.weblearn.exceptions.CategoryNotFoundException;
 import my.custom.learning.weblearn.repository.CategoryRepository;
@@ -45,10 +47,31 @@ public class CategoryController {
 		return category;
 	}
 	
-	@PostMapping(path = "/category/add", version=AppConstants.API_VERSION)
-	public ResponseEntity<CategoryOfProducts> addCategory(@Valid @RequestBody CategoryOfProducts category) {
+	@PostMapping(path = { "/category/upsert", "/category/upsert/{id}" }, version=AppConstants.API_VERSION)
+	public ResponseEntity<CategoryOfProducts> addCategory(@Valid @RequestBody CategoryOfProducts category, @PathVariable(required = false) Long id) {
+		Optional<CategoryOfProducts> savedCategory = java.util.Optional.empty();
+		if (id != null) {
+			category.setCategory_id(id);
+			savedCategory = repository.findById(id);
+
+			String categoryName = category.getCategory_name();
+			if (categoryName == null || categoryName.isEmpty()) {
+				category.setCategory_name(savedCategory.get().getCategory_name());
+			}
+
+			String categoryDescription = category.getDescription();
+			if (categoryDescription == null || categoryDescription.isEmpty()) {
+				category.setDescription(savedCategory.get().getDescription());
+			}
+		}
+		
 		CategoryOfProducts createdCatogory = repository.save(category);
 		URI location = URI.create("/category/"+createdCatogory.getCategory_id());
 		return ResponseEntity.created(location).build();
+	}
+	
+	@DeleteMapping(path = "/category/delete/{id}", version = AppConstants.API_VERSION)
+	public void deleteBrand(@PathVariable Long id) {
+		repository.deleteById(id);
 	}
 }
