6bcd8168e52b0c0ebc5dcca185ced66cf72da520
Mukherji, Sandeep <sandeep.talk@gmail.com>
2026-08-10 17:21:28 +0530

    feat: fetched the product information with the cart items

--- src/main/java/my/custom/learning/weblearn/entity/CartItem.java
+++ src/main/java/my/custom/learning/weblearn/entity/CartItem.java
@@ -1,9 +1,12 @@
 package my.custom.learning.weblearn.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -30,6 +33,18 @@ public class CartItem {
 	
 	@Positive(groups = Create.class, message = "Quantity must be a positive number, zero is not allowed")
 	private Integer quantity;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id", referencedColumnName = "product_id", insertable = false, updatable = false)
+	private Products product;
+
+	public Products getProduct() {
+		return product;
+	}
+
+	public void setProduct(Products product) {
+		this.product = product;
+	}
 
 	public CartItem() {
 		super();
